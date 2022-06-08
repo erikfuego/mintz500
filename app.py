aac27406a9ba7570ee4bf9f240d8e3d619a63954
Erik Rosales <rerick786@gmail.com>
2022-06-07 23:53:45 -0400

    still pushin P

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import hashlib
-import os
 
 from flask import (
     Flask,
@@ -7,7 +6,6 @@ from flask import (
     request,
     url_for,
     redirect,
-    make_response,
     session,
 )
 
@@ -18,68 +16,66 @@ from mintz500.models.User import User
 
 app = Flask(__name__)
 
-app.config.update(SECRET_KEY=os.urandom(24))
+app.config.update(SECRET_KEY="We mastered session")
 
 client = MongoClient("localhost", 27017, username="root", password="password")
 
 db = client.mintz500
 users = db.Users
 
+
 """
 I love ketchup
 """
-# Route for handling the login page logic
 @app.route("/", methods=["GET", "POST"])
 def login():
-    error = None
     if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        login_error = "Invalid Username or Password."
-        if not username or not password:
-            error = login_error
-            return render_template("index.html", error=error)
+        try:
+            username = request.form.get("username")
+            password = request.form.get("password")
 
-        hashed_password = hashlib.md5(
-            request.form.get("password").encode("utf8")
-        ).hexdigest()
-        user = db.Users.find_one({"username": username})
-        if not user['username'] or (hashed_password != user['password']):
-            error = login_error
-            return render_template("index.html", error=error)
+            Validator.validate_login(username, password)
 
-        session["username"] = user['username']
-        return redirect(url_for("home"))
+            user = get_user(username)
+            if user.password != hash_str(password):
+                raise UserNotFoundError
+
+            session["username"] = user.username
+            return redirect(url_for("home"))
+        except ValidationError as validation_error:
+            return render_template("login.html", error=str(validation_error))
+        except UserNotFoundError:
+            return render_template("login.html", error="Invalid username or password")
+
     elif request.method == "GET":
         signup_successful = session.get("signup_successful")
         message = None
         if signup_successful:
+            session.pop("signup_successful")
             message = "You successfully signed up!"
-        return render_template("index.html", message=message)
+        return render_template("login.html", message=message)
 
 
 @app.route("/home", methods=["GET"])
 def home():
     if request.method == "GET":
-        if session["username"]:
-            user = db.Users.find_one({"username": session["username"]})
+        if session.get("username"):
+            user = get_user(session["username"])
             return render_template("home.html", user=user)
         else:
-            return render_template("home.html")
+            return redirect(url_for("login"))
 
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    error = None
     if request.method == "POST":
         try:
             username = request.form["username"]
             password = request.form["password"]
-            user = User(username, password)
+            Validator.validate_signup(username, password)
 
-            Validator.validate_signup(user)
+            create_user(username, password)
 
-            create_user(user)
             session["signup_successful"] = True
             return redirect(url_for("login"))
         except ValidationError as validation_error:
@@ -87,26 +83,50 @@ def signup():
         except DuplicateKeyError:
             return render_template("signup.html", error="User already exists.")
     elif request.method == "GET":
-        return render_template("signup.html", error=error)
+        return render_template("signup.html")
 
 
-def create_user(user):
-    user.password = hashlib.md5(user.password.encode("utf8")).hexdigest()
+def create_user(username: str, password: str) -> User:
+    hashed_password = hash_str(password)
     users.insert_one(
-        {"_id": user.username, "username": user.username, "password": user.password}
+        {"_id": username, "username": username, "password": hashed_password}
     )
 
-    print("Successfully created user, " + user.username)
-    return user
+    print("Successfully created user, " + username)
+    return User(username, hashed_password)
+
+
+def get_user(username: str) -> User:
+    user_maybe = users.find_one(
+        {"_id": username}
+    )
+    if user_maybe is None:
+        raise UserNotFoundError
+    return User(user_maybe["username"], user_maybe["password"])
+
+
+def hash_str(password):
+    hashed_password = hashlib.md5(password.encode("utf8")).hexdigest()
+    return hashed_password
 
 
 class Validator:
     @staticmethod
-    def validate_signup(user):
+    def validate_signup(username: str, password: str):
         errors = []
-        if Validator.is_none_or_empty(user.username):
+        if Validator.is_none_or_empty(username):
             errors.append("username cannot be blank")
-        if Validator.is_none_or_empty(user.password):
+        if Validator.is_none_or_empty(password):
+            errors.append("password cannot be blank")
+        if len(errors):
+            raise ValidationError(errors)
+
+    @staticmethod
+    def validate_login(username: str, password: str):
+        errors = []
+        if Validator.is_none_or_empty(username):
+            errors.append("username cannot be blank")
+        if Validator.is_none_or_empty(password):
             errors.append("password cannot be blank")
         if len(errors):
             raise ValidationError(errors)
@@ -117,4 +137,8 @@ class Validator:
 
 
 class ValidationError(Exception):
+    pass
+
+
+class UserNotFoundError(Exception):
     pass
